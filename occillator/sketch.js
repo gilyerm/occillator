c0a53ce7c8b91a3c36f78e9ae93542f7aa587859
Gil Yermiyah <gilyerm@gmail.com>
2018-04-09 11:50:00 +0300

    update gil 2

--- occillator/sketch.js
+++ occillator/sketch.js
@@ -1,81 +1,65 @@
-
-let minH = 60;
-let maxH = 300;
-let totalW = 400;
+let minH;
+let maxH;
+let totalW ;
 let divC = 20;
-let divW = totalW / divC;
-let angOff;
+let divW = divC;
 
 let ang = 0;
 
 function setup()
 {
-  createCanvas(1000, 1000, WEBGL);
-  background(57);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  //background(57);
+  // divW=windowHeight/32;
+  // divC=windowWidth/32;
+  totalW=divC*divW;
+  minH=20;
+  maxH=pow(minH,2);
 
+  angOff = PI*sqrt(2)/150;
 
-  angOff = PI/100;
+  rectMode(CENTER);
+
+  directionalLight(154,228,176, -3*PI/4, 3*PI/4,-3*PI/4);
+  //ambientMaterial(100,100,100);
+  noStroke();
+  //rotate(3*PI/4,3*PI/4);
+
+  camera( (height/2.0), (height/2.0), (height/2.0) ,//camera position
+          0, 0, 0,///looking at
+          sin(90), sin(90), 0);
 }
 
 function draw()
 {
-  background(57);
-  rectMode(CENTER);
-  //translate(width/2, height/2);
+
   let curAng = ang;
+  background(250,250,250);
+
+
+  //translate(width/2, height/2);
+
   translate(-totalW/2, -totalW/2);
 
 
 
-  for (i=0; i<divW; i++)
+
+  for (i=0;i<divW; i+=1)
   {
-    for(j=0;j<divC;j++)
+    for(j=0;j<divC;j+=1)
     {
-      if(i<=divW/2)
-      {
-        if(j<=divC/2)
-        {
-          if(i+j<=divC/2)
-          {
-            continue;
-          }
-          curAng=ang+angOff*i*j;
-        }
-        else {
-          if(i+(divC-j)<=divC/2)
-          {
-            continue;
-          }
-          curAng=ang+angOff*i*(divC-j);
-        }
-      }
-      else
-      {
-        if(j<=divC/2)
-        {
-          if((divW-i)+j<=divC/2)
-          {
-            continue;
-          }
-          curAng=ang+angOff*(divW-i)*j;
-        }
-        else {
-          if((divW-i)+(divC-j)<=divC/2)
-          {
-            continue;
-          }
-          curAng=ang+angOff*(divW-i)*(divC-j);
-        }
-      }
-      noStroke();
-      fill(color(255*i/divW,255*j/divC, 255*i*j/divW));
+      curAng=(ang+angOff*
+        Math.sqrt(Math.pow(divW/2-i, 2) + Math.pow(divC/2-j, 2)))*2;
+
+
       push();
       let val = abs(sin(curAng));
       let h = map(val, 0, 1, minH, maxH);
-      translate(divW*i,divC * j);
-      rotateX(3*PI/4);
-      rotateY(3*PI/4);
-      box(divW,h,divC);
+      translate(divW*(i),divC*(j));///split
+      //rotateX(3*PI/4);
+      //rotateY(3*PI/4);
+      //rotateZ(3*PI/4);
+      box(divC,divW,h);
       pop();
     }
   }
