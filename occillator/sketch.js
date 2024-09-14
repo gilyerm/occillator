c7c41af46ee59b8a3e6a3d624c7c7825d6d58bc4
Gil_Yermiyah <gilyerm@gmail.com>
2024-09-14 12:39:38 +0300

    occillator 2.0
    Update p5.js library to version 1.10.0
    refactor code for improved readability
    now it look like in the gif
    
    TODO: optimize

--- occillator/sketch.js
+++ occillator/sketch.js
@@ -1,47 +1,76 @@
-let minH;
-let maxH;
-let totalW ;
-let divC = 32;
-let divW = divC;
-let pixel=16;
-let ang = 0;
+const boxWidth = 32;
+const boxesPerRow = 16;
+const canvasWidth = boxWidth * boxesPerRow;
+const canvasHeight = canvasWidth; // Make the canvas square
+const boxMinHeight = canvasHeight / 8;
+const boxMaxHeight = canvasHeight;
+const colors = [
+  [130, 186, 180],
+  [230, 228, 176],
+  [63, 84, 132],
+];
+let angle = 0; // Angle for the sine wave
+let maxDistance;
 
-function setup()
-{
-  totalW=divC*divW;
-  minH=pixel;
-  maxH=pow(minH,2);
-  angOff = PI/divC;
-  createCanvas(divW*(pixel),divC*(pixel), WEBGL);
-  rectMode(CENTER);
+function setup() {
+  createCanvas(canvasWidth, canvasHeight, WEBGL);
   noStroke();
 
-  camera((height/1.6), (height/1.6), (height/0.8) ,//camera position
-          0,0,0,            ///center of the sketch
-          sin(90), sin(90), 0);///shift camera
+  maxDistance = dist(0, 0, width / 2, height / 2); // Calculate the maximum distance from the center of the canvas
 }
 
-function draw()
-{
-  background(240,240,240);
-  translate(-totalW/3, -totalW/3);
-  for (i=0;i<divW; i++)
-  {
-    for(j=0;j<divC;j++)
-    {
-      val=sin(ang-pythagorean(divW/2-i, divC/2-j)/3);
-      let h = map(val, -1, 1, minH, maxH);
-      push();
-      translate(pixel*(i),pixel*(j));///split
-      fill(color(255*(h)/(maxH),255*i/divW,255*j/divC));
-      box(pixel,pixel,h);
-      pop();
+function draw() {
+  clear(); // Clear the canvas before drawing the next frame
+  ortho(-width, width, -height, height); // Set the orthographic projection
+
+  rotateX(-QUARTER_PI / 1.25); // Rotate the canvas around the x-axis
+  rotateY(QUARTER_PI); // Rotate the canvas around the y-axis
+  
+  for (let z = 0; z < height; z += boxWidth) { // Loop through the depth of the canvas with a step of boxWidth
+    for (let x = 0; x < width; x += boxWidth) { // Loop through the width of the canvas with a step of boxWidth
+      push(); // Save the current transformation matrix
+
+      const distance = dist(x + boxWidth / 2, z + boxWidth / 2, width / 2, height / 2); 
+      // Calculate the distance from the center of the canvas from the center of the rectangle
+      const offset = map(distance, 0, maxDistance, -PI, PI); // Map the distance to an offset angle
+      const boxHight = map(sin(angle + offset), -1, 1, boxMinHeight, boxMaxHeight); // Map the sine value to the height of the rectangle
+
+      translate(x - (width / 2), 0, z - (height / 2)); // Translate the origin to the center of the rectangle
+
+      drawBox(boxWidth, boxHight, boxWidth); // Draw the box with different colors on each side
+      pop(); // Restore the transformation matrix
     }
   }
-  ang += angOff;
-  ang %=TWO_PI;
+  
+  angle = (angle - 0.1) % TWO_PI; // Update angle for animation
 }
 
-function pythagorean(sideA, sideB){
-  return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+function drawBox(w, h, d) { // Function to draw a box with different colors on each side
+  // Define the 8 vertices of the cube
+  const vertices = [
+    [-w / 2, -h / 2, -d / 2], // 0
+    [w / 2, -h / 2, -d / 2],  // 1
+    [w / 2, h / 2, -d / 2],   // 2
+    [-w / 2, h / 2, -d / 2],  // 3
+    [-w / 2, -h / 2, d / 2],  // 4
+    [w / 2, -h / 2, d / 2],   // 5
+    [w / 2, h / 2, d / 2],    // 6
+    [-w / 2, h / 2, d / 2],    // 7
+  ];
+
+  // Use a loop to define each face and color to reduce redundant code
+  const faces = [
+    { indices: [...vertices[0], ...vertices[1], ...vertices[5], ...vertices[4]], color: colors[0] },
+    { indices: [...vertices[4], ...vertices[5], ...vertices[6], ...vertices[7]], color: colors[1] },
+    { indices: [...vertices[0], ...vertices[3], ...vertices[7], ...vertices[4]], color: colors[2] },
+    // { indices: [...vertices[1], ...vertices[2], ...vertices[6], ...vertices[5]], color: colors[2] },
+    // { indices: [...vertices[0], ...vertices[1], ...vertices[2], ...vertices[3]], color: colors[1] },
+    // { indices: [...vertices[2], ...vertices[3], ...vertices[7], ...vertices[6]], color: colors[0] },
+  ];
+
+  // Draw each face of the box
+  faces.forEach(face => {
+    fill(...face.color);
+    quad(...face.indices);
+  });
 }
